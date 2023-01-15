7219e270f59bd34227fcf878fa137b6ad40b8eb7
Hannah-2456 <118482509+Hannah-2456@users.noreply.github.com>
2023-01-15 18:09:18 +0100

    Align Design to Figma prototype

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 let signupBtn = document.getElementById("signupBtn")
 let signinBtn = document.getElementById("signinBtn")
-let nameField = document.getElementById("nameField")
+let firstNameField = document.getElementById("firstNameField")
+let lastNameField = document.getElementById("lastNameField")
+let confirmPasswordField = document.getElementById("confirmPasswordField")
 let title = document.getElementById("title")
+let intro = document.getElementById("intro")
 
 signinBtn.onclick = function(){
-    nameField.style.maxHeight = "0";
-    title.innerHTML = "Sign In";
+    firstNameField.style.maxHeight = "0";
+    lastNameField.style.maxHeight = "0";
+    confirmPasswordField.style.maxHeight = "0";
+    title.innerHTML = "Log In";
+    intro.innerHTML = "Welcome to Amica";
     signupBtn.classList.add("disable");
     signinBtn.classList.remove("disable");
 }
 
 signupBtn.onclick = function(){
-    nameField.style.maxHeight = "60px";
+    firstNameField.style.maxHeight = "60px";
+    lastNameField.style.maxHeight = "60px";
+    confirmPasswordField.style.maxHeight = "60px";
     title.innerHTML = "Sign Up";
+    intro.innerHTML = "Join us and find new connections in your area. Create your free Amica account";
     signupBtn.classList.remove("disable");
     signinBtn.classList.add("disable");
 }
