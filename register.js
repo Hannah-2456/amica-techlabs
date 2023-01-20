5946e03e67504eadd5f7d0fb0ae05e7a698cfea7
Rolandio98 <rolandulfstedt@gmail.com>
2023-01-20 14:42:32 +0100

    gender info to user object

--- register.js
+++ register.js
@@ -10,6 +10,9 @@ function editUser(event) {
     let birthday = document.getElementById("birthday").value;
     objectUser['birthday'] = birthday.toString();
 
+    let gender = document.querySelector('input[name="gender"]:checked').value;
+    objectUser['gender'] = gender;
+
 
 
     localStorage.setItem(objectUser.userName, JSON.stringify(objectUser));
