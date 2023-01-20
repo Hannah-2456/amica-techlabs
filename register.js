14d836ea64986bd4aac3a6948fb1b6dc3130cfcc
Rolandio98 <rolandulfstedt@gmail.com>
2023-01-20 14:28:29 +0100

    birthday info added to user object

--- register.js
+++ register.js
@@ -1,9 +1,20 @@
-const detailsForm = document.getElementById("detailsForm")
-detailsForm.addEventListener('submit', editUser)
+const detailsForm = document.getElementById("detailsForm");
+detailsForm.addEventListener('submit', editUser);
 
 
 function editUser(event) {
-    event.preventDefault()
+    event.preventDefault();
 
-    console.log("test")
+    var objectUser = JSON.parse(localStorage.getItem(sessionStorage.getItem("currentUser")));
+
+    let birthday = document.getElementById("birthday").value;
+    objectUser['birthday'] = birthday.toString();
+
+
+
+    localStorage.setItem(objectUser.userName, JSON.stringify(objectUser));
+
+    console.log("done");
+
+
 }
