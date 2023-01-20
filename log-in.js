5f873884ce76fcab8dec6083b59fe5fcd205df92
Hannah-2456 <118482509+Hannah-2456@users.noreply.github.com>
2023-01-20 23:12:47 +0100

    Migration

---
+++ log-in.js
@@ -0,0 +1,28 @@
+const logInForm = document.getElementById("logInForm")
+logInForm.addEventListener('submit', checkUser)
+
+function checkUser(event) {
+    event.preventDefault()
+
+    var usernameInput = document.getElementById("usernameInput-logIn").value;
+
+    sessionStorage.setItem("currentUser", usernameInput);
+    
+    var userFromLocalStorage = JSON.parse(localStorage.getItem(usernameInput));
+
+    if(!userFromLocalStorage) {
+        var usernameError = document.getElementById("usernameError");
+        usernameError.innerHTML = "User does not exist. Please try again.";
+    }
+
+    var passwordInput = document.getElementById("passwordInput-logIn").value;
+
+    if(userFromLocalStorage.password !== passwordInput) {
+        var passwordError = document.getElementById("passwordError");
+        passwordError.innerHTML = "Invalid password. Please try again.";
+
+    } else {
+        console.log('Logged in successfully.')
+        location.href = "home.html";
+    }
+}
