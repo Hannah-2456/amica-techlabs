56d298d66e08b1495d8398a5543fc1c09b6640b3
Rolandio98 <rolandulfstedt@gmail.com>
2023-01-23 13:33:17 +0100

    index.js not needed
    otherwise log-in page omitted

--- index.js
+++
@@ -1,7 +0,0 @@
-
-//Redirect logged-in users
-if (window.localStorage.getItem("currentUser")) {
-    window.location = "home.html"
-} else if (window.localStorage.getItem("registeringUser")) {
-    window.location = "register.html"
-}
